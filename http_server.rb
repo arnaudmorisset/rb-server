e012df3cc94e002b2cdc1f01cdccaece572d73c8
Arnaud Morisset <arnaudmorisset@protonmail.com>
2022-04-06 17:48:22 +0200

    Prototype a basic Thin abstraction

--- http_server.rb
+++ http_server.rb
@@ -1,11 +1,27 @@
 # frozen_string_literal: true
 
+require 'thin'
+
 # Custom HTTP server for any kind of request
 # that shouldn't go through the API gateway.
 class HTTPServer
-  class << self
-    def greetings
-      'Hello, World!'
-    end
+  attr_reader :domain, :port, :server
+
+  def initialize(domain, port)
+    @port = port
+    @domain = domain
+    @server = Thin::Server.new(domain, 8080, Rack::URLMap.new)
+  end
+
+  def handle(path, handler) end
+
+  def start
+    @thread = Thread.new { @server.start }
+    sleep(0.5) until @server.running?
+  end
+
+  def stop
+    @server.stop
+    @thread.exit
   end
 end
--- http_server_test.rb
+++ http_server_test.rb
@@ -1,18 +1,44 @@
 # frozen_string_literal: true
 
+require 'net/http'
 require 'minitest/autorun'
 require_relative './http_server'
 
+# Basic endpoint returning status code 200 and body 'OK!'
+class PingHandler
+  def call(_)
+    [
+      200,
+      {},
+      ['OK!']
+    ]
+  end
+end
+
 # Required tests for our custom HTTP server
-class HTTPServerTest < Minitest::Test
-  def test_greetings
-    assert_equal HTTPServer.greetings, 'Hello, World!'
+describe 'functionnal test for our custom HTTP server' do
+  before(:all) do
+    @server = HTTPServer.new('0.0.0.0', 8080)
+    @server.server.silent = true
+    @server.handle('/ping', PingHandler)
   end
 
-  # TCP Socket listening on given port
-  # HTTP Server handle a given route
-  # HTTP Server send back 404 when route doesn't exist
-  # HTTP Server handle fatal errors and returns 500
-  # HTTP Server answer on /health by default
-  # HTTP Server answer on a given /health endpoint
+  after(:all) do
+    @server.stop
+  end
+
+  it 'test that server is listening on given port' do
+    @server.start
+
+    # No need for assertion as this call will raise an exception
+    # if Net library is unable to open a TCP client socket
+    # on the given domain:port address.
+    Net::HTTP.get('0.0.0.0', '/', 8080)
+  end
 end
+
+# HTTP Server handle a given route
+# HTTP Server send back 404 when route doesn't exist
+# HTTP Server handle fatal errors and returns 500
+# HTTP Server answer on /health by default
+# HTTP Server answer on a given /health endpoint
