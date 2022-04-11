9d0916f58103ab0d3e1842b266299c618efc9094
Arnaud Morisset <arnaudmorisset@protonmail.com>
2022-04-11 11:23:05 +0200

    Test 500 errors

--- http_server_test.rb
+++ http_server_test.rb
@@ -15,6 +15,13 @@ class PingHandler
   end
 end
 
+# Basic endpoint raising an error (to test error recovery)
+class ErrorHandler
+  def call(_)
+    raise StandardError
+  end
+end
+
 # Required tests for our custom HTTP server
 describe 'functional test for our custom HTTP server' do
   before(:all) do
@@ -23,15 +30,6 @@ describe 'functional test for our custom HTTP server' do
 
   after do
     @server.stop
-  end
-
-  it 'test that server is listening on given port' do
-    @server.start
-
-    # No need for assertion as this call will raise an exception
-    # if Net library is unable to open a TCP client socket
-    # on the given domain:port address.
-    Net::HTTP.get('0.0.0.0', '/', 8080)
   end
 
   it 'test that server is able to handle a given route' do
@@ -52,8 +50,17 @@ describe 'functional test for our custom HTTP server' do
     assert_equal Net::HTTPNotFound, res.code_type
     assert_equal 'Not Found: /ping', res.body
   end
+
+  it 'test that server is able to recover from an error and return 500' do
+    @server.handle('/bad', ErrorHandler)
+    @server.start
+
+    res = Net::HTTP.get_response(URI('http://0.0.0.0:8080/bad'))
+
+    assert_equal Net::HTTPInternalServerError, res.code_type
+    assert_equal 'Internal server error', res.body
+  end
 end
 
-# HTTP Server handle fatal errors and returns 500
 # HTTP Server answer on /health by default
 # HTTP Server answer on a given /health endpoint
